ae1c854c544c18e7a0dd3282036a2cbbcf09bb0a
Julian Schiavo <justdotJS@icloud.com>
2020-04-07 12:47:44 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [
         .iOS(.v8),
         .tvOS(.v9),
-        .macOS(.v10_15)
+        .macOS(.v10_14)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
